403b6c84dfbb5c7d05c8e4f19ea99c361ec8e05d
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-01-08 09:15:03 -0500

    update

--- components/Home/Bars.tsx
+++ components/Home/Bars.tsx
@@ -1,15 +1,39 @@
-import { Box } from "@chakra-ui/react";
-
-const Bar = () => {
-    return <Box>bar</Box>;
+import { Box, Flex } from "@chakra-ui/react";
+interface BarProps {
+    i: number;
+}
+const Bar = ({ i }: BarProps) => {
+    return (
+        <Flex
+            position="absolute"
+            bottom={`${-50}%`}
+            right={`calc(20% * ${i} - 5rem)`}
+            width="10%"
+            height="10%"
+            borderRadius="50rem"
+            bg="linear-gradient(255deg, #6c3ca8,#00000001,#00000001)"
+            transform="rotate(30deg)"
+            display="flex"
+            justify="center"
+            align="center"
+        >
+            bar
+        </Flex>
+    );
 };
 
 const Bars = () => {
     return (
-        <Box>
-            <Bar />
-            <Bar />
-            <Bar />
+        <Box
+            align-items="flex-end"
+            position="relative"
+            width="75%"
+            margin-right="5%"
+            min-height="50rem"
+        >
+            <Bar i={1} />
+            <Bar i={2} />
+            <Bar i={3} />
         </Box>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,26 @@ import Bars from "../components/Home/Bars";
 
 const Home = () => {
     return (
-        <Box as="main">
-            <Box as="section" h="100vh">
+        <Box
+            as="main"
+            color="aliceblue"
+            display="flex"
+            justify-content="right"
+            position="relative"
+            flexDirection="column"
+        >
+            <Box
+                as="section"
+                h="100vh"
+                w="100%"
+                position="relative"
+                display="flex"
+                flexDirection="column"
+                justifyContent="left"
+                alignItems="center"
+                overflow="hidden"
+                background="linear-gradient(to bottom right, rgb(12, 12, 12), 75%, #473361)"
+            >
                 <Bars />
             </Box>
             <Box as="section" h="100vh">
