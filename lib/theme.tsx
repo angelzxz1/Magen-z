0941a3a1d465bb0c56e0693f09fb48626d962a03
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-01-19 22:12:52 -0500

    Added a transparent version of purple and teal

--- lib/theme.tsx
+++ lib/theme.tsx
@@ -8,6 +8,19 @@ const config = {
     disableTransitionOnChange: false,
 };
 const colors = {
+    purpleTransparent: {
+        50: "#dccfed28",
+        100: "#c0a5e428",
+        200: "#a47bda28",
+        300: "#8851d128",
+        400: "#6c26c728",
+        500: "#541d9c28",
+        600: "#42167b28",
+        700: "#300f5a28",
+        800: "#1e083928",
+        900: "#0b011828",
+    },
+
     purple: {
         50: "#dccfed",
         100: "#c0a5e4",
@@ -20,7 +33,18 @@ const colors = {
         800: "#1e0839",
         900: "#0b0118",
     },
-
+    tealTransparent: {
+        50: "#edfcf728",
+        100: "#bcefde28",
+        200: "#8ae2c528",
+        300: "#58d5ac28",
+        400: "#26c79228",
+        500: "#1d9c7228",
+        600: "#167c5a28",
+        700: "#0f5b4228",
+        800: "#083b2a28",
+        900: "#011a1128",
+    },
     teal: {
         50: "#edfcf7",
         100: "#bcefde",
